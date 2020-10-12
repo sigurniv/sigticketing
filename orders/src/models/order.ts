ee7b8f0284204a04612c24b896c1a5731ab6b9e2
sigurniv <sigurniv@gmail.com>
2020-10-12 17:48:06 +0300

    add orders service routes

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
+import { OrderStatus } from "@sigticketing/common";
+import { TicketDoc } from "./ticket";
 
 interface OrderAttrs {
     userId: string;
-    status: string;
+    status: OrderStatus;
     expiresAt: Date;
     ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
     userId: string;
-    status: string;
+    status: OrderStatus;
     expiresAt: Date;
     ticket: TicketDoc;
 }
@@ -25,7 +27,9 @@ const orderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        require[vd: true
+        required: true,
+        enum: Object.values(OrderStatus),
+        default: OrderStatus.Created
     },
     expiresAt: {
         type: mongoose.Schema.Types.Date
@@ -38,7 +42,15 @@ const orderSchema = new mongoose.Schema({
 }, {
     toJSON: {
         transform(doc, ret) {
-
+            ret.id = ret._id;
+            delete ret._id;
         }
     }
 })
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order(attrs)
+}
+
+export const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema)
+export { OrderStatus }
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express'
-import { requireAuth, validateRequest } from "@sigticketing/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@sigticketing/common";
 import { body } from 'express-validator';
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post('/api/orders', requireAuth, [
         body('ticketId')
@@ -12,7 +16,36 @@ router.post('/api/orders', requireAuth, [
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        res.send({})
+        const { ticketId } = req.body;
+
+        //Find the ticket user is trying to order in the database
+        const ticket = await Ticket.findById(ticketId)
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        //Make sure ticket is not reserved
+        const isReserved = await ticket.isReserved()
+        if (isReserved) {
+            throw new BadRequestError('ticket is already reserved')
+        }
+
+        //Calculate an expiration date for the order
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+        //Build the order and save to the database
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket
+        })
+
+        await order.save()
+
+        //Publish and event saying that order was created
+        res.status(201).send(order)
     })
 
 export { router as newOrderRouter }
