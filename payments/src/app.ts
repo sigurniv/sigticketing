9b8782f2e4cbf491b7cf052e73e9380b6ce6d1da
sigurniv <sigurniv@gmail.com>
2020-10-13 16:18:41 +0300

    add payment service

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,31 +1,24 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import { errorHandler, NotFoundError, currentUser } from "@sigticketing/common";
-import cookieSession from "cookie-session";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
-app.use(cookieSession({
+app.use(
+  cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
-}))
-
-app.use(currentUser)
-app.use(createTicketRouter)
-app.use(showTicketRouter)
-app.use(indexTicketRouter)
-app.use(updateTicketRouter)
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
 
 app.all('*', async (req, res) => {
-    throw new NotFoundError()
-})
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
-export { app }
+export { app };
